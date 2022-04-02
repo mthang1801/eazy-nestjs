b6e1dfe41f406a789b39b6e6c4911c9995543169
mthang1801 <mthang1801@gmail.com>
2022-04-02 20:11:57 +0700

    update order_status

--- src/app/dto/orderStatus/create-orderStatus.dto.ts
+++ src/app/dto/orderStatus/create-orderStatus.dto.ts
@@ -3,53 +3,70 @@ export class OrderStatusCreateDTO {
   @IsNotEmpty()
   @MaxLength(1)
   status: string;
+
   @IsNotEmpty()
   @MaxLength(1)
   type: string;
+
   @IsOptional()
   @MaxLength(1)
   is_default: string;
+
   @IsOptional()
   position: number;
 
   @IsOptional()
   description: string;
+
   @IsOptional()
   email_subj: string;
+
   @IsNotEmpty()
   email_header: string;
+
   @IsOptional()
   lang_code: string;
 
   @IsOptional()
   param: number;
+
   @IsOptional()
   value: number;
+
+  @IsOptional()
+  color_value: string;
 }
 export class orderStatusUpdateDTO {
   @IsOptional()
   @MaxLength(1)
   status: string;
+
   @IsOptional()
   @MaxLength(1)
   type: string;
+
   @IsOptional()
   @MaxLength(1)
   is_default: string;
+
   @IsOptional()
   position: number;
 
   @IsOptional()
   description: string;
+
   @IsOptional()
   email_subj: string;
+
   @IsOptional()
   email_header: string;
+
   @IsOptional()
   lang_code: string;
 
   @IsOptional()
   param: number;
+
   @IsOptional()
   value: number;
 }
