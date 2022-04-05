e79907db9432237ffd42a49d6e2261f9e9fb1188
mthang1801 <mthang1801@gmail.com>
2022-04-05 11:48:44 +0700

    adding update accessory itg

--- src/app/controllers/integration/promotionAccessory.controller.ts
+++ src/app/controllers/integration/promotionAccessory.controller.ts
@@ -7,19 +7,51 @@ export class PromotionAccessoryItgController extends BaseController {
   constructor(private service: PromotionAccessoryService) {
     super();
   }
-  @Post('/gift')
-  async create(@Res() res: Response, @Body() data) {
+  @Post('/free')
+  async createFree(@Res() res: Response, @Body() data) {
     await this.service.itgCreate(data, 2);
     return this.responseSuccess(res, null, 'Thành công.');
   }
 
-  @Put('gift/:app_core_id')
-  async udpate(
+  @Put('free/:app_core_id')
+  async updateFree(
     @Res() res: Response,
     @Body() data,
     @Param('app_core_id') app_core_id: string,
   ) {
     await this.service.itgUpdate(app_core_id, data, 2);
+    return this.responseSuccess(res, null, 'Thành công.');
+  }
+
+  @Post('/promotion')
+  async createPromotion(@Res() res: Response, @Body() data) {
+    await this.service.itgCreate(data, 1);
+    return this.responseSuccess(res, null, 'Thành công.');
+  }
+
+  @Put('promotion/:app_core_id')
+  async updatePromotion(
+    @Res() res: Response,
+    @Body() data,
+    @Param('app_core_id') app_core_id: string,
+  ) {
+    await this.service.itgUpdate(app_core_id, data, 1);
+    return this.responseSuccess(res, null, 'Thành công.');
+  }
+
+  @Post('/warranty-packages')
+  async createWarranty(@Res() res: Response, @Body() data) {
+    await this.service.itgCreate(data, 3);
+    return this.responseSuccess(res, null, 'Thành công.');
+  }
+
+  @Put('warranty-packages/:app_core_id')
+  async updateWarranty(
+    @Res() res: Response,
+    @Body() data,
+    @Param('app_core_id') app_core_id: string,
+  ) {
+    await this.service.itgUpdate(app_core_id, data, 3);
     return this.responseSuccess(res, null, 'Thành công.');
   }
 }
