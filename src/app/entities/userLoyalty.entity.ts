029771960f16a87b7ee09e593ebe36f5689c60a3
mthang1801 <mthang1801@gmail.com>
2022-04-06 17:31:28 +0700

    fix customer lasted buy

--- src/app/entities/userLoyalty.entity.ts
+++ src/app/entities/userLoyalty.entity.ts
@@ -3,6 +3,4 @@ import { convertToMySQLDateTime } from 'src/utils/helper';
 export class UserLoyaltyEntity {
   user_id: number = 0;
   loyalty_point: number = 0;
-  created_at: string = convertToMySQLDateTime();
-  updated_at: string = convertToMySQLDateTime();
 }
