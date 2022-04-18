443e4d5df53adcf74e03c6c2860fb09f9f4db337
mthang1801 <mthang1801@gmail.com>
2022-04-18 11:37:40 +0700

    add product in category

--- src/app/dto/auth/auth-login.dto.ts
+++ src/app/dto/auth/auth-login.dto.ts
@@ -6,13 +6,8 @@ export class LoginDto {
   email: string;
 
   @IsOptional()
-  @Matches(/(84|0[3|5|7|8|9])+([0-9]{8})\b/, { message: 'Phone is invalid' })
   phone: string;
 
   @IsNotEmpty()
-  @Matches(/^(?=.*[A-Za-z])(?=.*\d)(?=.*[@$!%*#?&])[A-Za-z\d@$!%*#?&]{8,}$/, {
-    message:
-      'Password need to get minimum eight characters, at least one letter, one number and one special character:',
-  })
   password: string;
 }
