6e8c0ed0a19879d7764d108756a37204a1d2a49c
mthang1801 <mthang1801@gmail.com>
2022-05-13 08:31:47 +0700

    update

--- src/app/entities/roleFunction.entity.ts
+++ src/app/entities/roleFunction.entity.ts
@@ -3,7 +3,6 @@ export class RoleFunctionEntity {
   role_funct_id: number = 0;
   role_id: number = 0;
   funct_id: number = 0;
-  permission: number = 1;
   created_at: string = formatStandardTimeStamp();
   created_by: number = 0;
   updated_at: string = formatStandardTimeStamp();
