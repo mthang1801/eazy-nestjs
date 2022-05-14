72fe2765de1d8f3235572864d9f3f7a1f3a2eaff
mthang1801 <mthang1801@gmail.com>
2022-05-14 09:06:50 +0700

    update valuation bill

--- src/app/entities/valuationBillCriteriaDetail.entity.ts
+++ src/app/entities/valuationBillCriteriaDetail.entity.ts
@@ -2,4 +2,5 @@ export class ValuationBillCriteriaDetailEntity {
   valuation_bill_detail_id: number = 0;
   valuation_bill_id: number = 0;
   criteria_detail_id: number = 0;
+  criteria_id: number = 0;
 }
