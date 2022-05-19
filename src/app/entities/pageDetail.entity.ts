b92ad2279f122b2b9c5a205db81b08f949f72b18
mthang1801 <mthang1801@gmail.com>
2022-05-19 15:09:46 +0700

    update product list BE

--- src/app/entities/pageDetail.entity.ts
+++ src/app/entities/pageDetail.entity.ts
@@ -1,6 +1,7 @@
 export class PageDetailEntity {
   page_detail_id: number = 0;
   page_id: number = 0;
+  page_detail_data: string;
   module_name: string = '';
   description: string = '';
   position: number = null;
