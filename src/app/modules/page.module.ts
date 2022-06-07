e8d41c31f236bd4f46f35e48217261d722824e84
mthang1801 <mthang1801@gmail.com>
2022-06-07 09:16:03 +0700

    update

--- src/app/modules/page.module.ts
+++ src/app/modules/page.module.ts
@@ -1,5 +1,5 @@
 import { Module } from '@nestjs/common';
-import { PageController } from '../controllers/be/v1/page.controller';
+import { PageController as PageControllerBE } from '../controllers/be/v1/page.controller';
 import { PageService } from '../services/page.service';
 import { PageRepository } from '../repositories/page.repository';
 import { PageDetailRepository } from '../repositories/pageDetail.repository';
@@ -12,7 +12,7 @@ import { BannerModule } from './banner.module';
 import { FlashSaleModule } from './flashSale.module';
 
 @Module({
-  controllers: [PageController, PageControllerTester, PageControllerFE],
+  controllers: [PageControllerBE, PageControllerTester, PageControllerFE],
   providers: [
     PageService,
     PageRepository,
